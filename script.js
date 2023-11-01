9e6411815b89b55de6f4b49899a30934b50d3489
Rachel Lawson <rach.marie.lawson@gmail.com>
2023-11-01 13:53:29 -0700

    Add a button that prompts user for new grid dimensions

--- script.js
+++ script.js
@@ -4,14 +4,30 @@
     - put inside container div
     - careful with borders and margins affectng size of boxes
 2. Change div background color when mouseover
-    
+3. Add button to trigger a popup asking for number of squares per side for new grid. Replace old grid with new (takes up same total space as
+    previous grid). User input has max of 100. (hint: research button tags and prompts)
+
 */
+
+let btnContainer = document.createElement('div');
+btnContainer.classList.add('btnContainer');
+document.body.insertBefore(btnContainer, document.body.firstChild);
+let newGridBtn = document.createElement('button');
+newGridBtn.classList.add('newGridBtn');
+let btnText = document.createTextNode("Create New Grid");
+newGridBtn.appendChild(btnText);
+newGridBtn.addEventListener('click', function() {
+    // Display the prompt when the button is clicked
+    const userInput = prompt('Please enter the number of squares per side: ');
+    console.log(userInput);
+    });
+btnContainer.appendChild(newGridBtn);
 
 let gridContainer = document.getElementById('gridContainer');
 
-function buildGrid(numberOfSquares) {
+function buildGrid(squaresPerSide) {
+    let numberOfSquares = squaresPerSide * squaresPerSide;
     for (let i = 0; i < numberOfSquares; i++) {
-        
 
         let gridSquare = document.createElement('div'); 
         gridSquare.classList.add('square'); 
@@ -26,4 +42,4 @@ function buildGrid(numberOfSquares) {
     }
 }
 
-buildGrid(256);
+buildGrid(16);
